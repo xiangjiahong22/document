cdc4ab38ad6e5f4ddf30fd48fc2d2e15830ae86c
xiangjiahong22 <541353071@qq.com>
2018-02-26 13:05:03 +0800

    Add files via upload

---
+++ 20150618财务系统副本/js/common.js
@@ -0,0 +1,52 @@
+(function($){
+	$().ready(function(){
+		var winScreen=window.screen.width;
+		if(winScreen>=640){
+			winScreen=320;
+		}
+		$('html').css('font-size',winScreen/10+'px');
+
+
+		$(".proListCon .imgStyle").each(function(i){
+	 		$(".proListCon .imgStyle").eq(i).addClass('animated zoomIn');
+	 	});
+
+	 	$('.proListCon .trigStyle').attr('bBtn','0');
+
+		$('.proListCon .proList a').on('click',function(){
+			if($('.trigStyle',this).attr('bBtn')=='0'){
+				$('.trigStyle',this).css({
+					'-webkit-transform':"rotateZ(360deg)"
+				})
+
+				$('.trigStyle',this).attr('bBtn','1');
+			}else{
+				$('.trigStyle',this).css({
+					'-webkit-transform':"rotateZ(0deg)"
+				})
+				$('.trigStyle',this).attr('bBtn','0');
+			}
+		});
+	});
+
+
+
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
